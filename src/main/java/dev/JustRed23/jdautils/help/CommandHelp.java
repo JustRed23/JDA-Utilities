72d33b5cdb2216eb4ee8af0ca32f38e381b84cd2
JustRed23 <manuel.bormans@gmail.com>
2023-10-02 13:46:55 +0200

    Add documentation

--- src/main/java/dev/JustRed23/jdautils/help/CommandHelp.java
+++ src/main/java/dev/JustRed23/jdautils/help/CommandHelp.java
@@ -8,13 +8,22 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.stream.Stream;
 
+/**
+ * A class to help with the creation of help commands for slash commands
+ */
 public final class CommandHelp {
 
     private static EmbedBuilder DEFAULT_BUILDER = new EmbedBuilder()
             .setColor(0x00ff00)
             .setFooter("Help provided by JDA-Utilities", JDAUtilities.getBotIconUrl());
 
-
+    /**
+     * Gets help for a specific command
+     * @param commandName The name of the command to get help for
+     * @return An EmbedBuilder containing the help for the command, or empty if the command was not found
+     * @see EmbedBuilder#isEmpty()
+     * @see #setDefaultBuilder(EmbedBuilder)
+     */
     public static EmbedBuilder getCommandHelp(String commandName) {
         Checks.notEmpty(commandName, "Command name");
         EmbedBuilder builder = makeCommandBuilder();
@@ -22,7 +31,14 @@ public final class CommandHelp {
         return builder;
     }
 
+    /**
+     * Gets help for a specific command
+     * @param commandName The name of the command to get help for
+     * @param builder The EmbedBuilder to use for the help <br><b>NOTE - The content of the builder will be overridden</b>
+     * @see EmbedBuilder#isEmpty()
+     */
     public static void getCommandHelp(String commandName, @NotNull EmbedBuilder builder) {
+        builder.clearFields();
         getCommands().filter(cmd -> cmd.getName().equalsIgnoreCase(commandName))
                 .findFirst()
                 .ifPresent(cmd -> {
@@ -35,6 +51,11 @@ public final class CommandHelp {
                 });
     }
 
+    /**
+     * Gets help for all commands
+     * @return An EmbedBuilder containing the names and descriptions of all commands
+     * @see #setDefaultBuilder(EmbedBuilder)
+     */
     public static EmbedBuilder getAllCommands() {
         EmbedBuilder builder = makeCommandBuilder();
         builder.setTitle("Showing list of all commands");
@@ -42,13 +63,28 @@ public final class CommandHelp {
         return builder;
     }
 
+    /**
+     * Gets help for all commands
+     * @param builder The EmbedBuilder to use for the help <br><b>NOTE - The content of the builder will be overridden</b>
+     * @see #setDefaultBuilder(EmbedBuilder)
+     */
     public static void getAllCommands(@NotNull EmbedBuilder builder) {
         if (getCommands().findAny().isEmpty()) {
             builder.setDescription("No commands found");
             return;
         }
 
+        builder.clearFields();
         getCommands().forEach(cmd -> builder.addField(cmd.getName(), cmd.getDescription(), false));
+    }
+
+    /**
+     * Sets the default EmbedBuilder to use for help commands, this can be used to set the color, footer, etc.
+     * @param builder The EmbedBuilder to use
+     */
+    public static void setDefaultBuilder(@NotNull EmbedBuilder builder) {
+        Checks.notNull(builder, "EmbedBuilder");
+        DEFAULT_BUILDER = builder;
     }
 
     private static Stream<SlashCommandData> getCommands() {
@@ -56,11 +92,6 @@ public final class CommandHelp {
                 .stream()
                 .filter(cmd -> cmd instanceof SlashCommandData)
                 .map(cmd -> (SlashCommandData) cmd);
-    }
-
-    public static void setDefaultBuilder(@NotNull EmbedBuilder builder) {
-        Checks.notNull(builder, "EmbedBuilder");
-        DEFAULT_BUILDER = builder;
     }
 
     private static EmbedBuilder makeCommandBuilder() {
