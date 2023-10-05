634f12a01782bab6d22d9715ac930bd804f5d5d0
JustRed23 <manuel.bormans@gmail.com>
2023-10-05 16:42:51 +0200

    Optimize imports

--- src/main/java/dev/JustRed23/jdautils/message/MessageComponent.java
+++ src/main/java/dev/JustRed23/jdautils/message/MessageComponent.java
@@ -8,8 +8,6 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Function;
-
 /**
  * <b>Internal use only</b><br>
  * Used to register message events to an {@link EventWatcher}
