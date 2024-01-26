0b9ea16d03f371b14bcc086c7e78652dbbb46efb
JustRed23 <manuel.bormans@gmail.com>
2024-01-26 16:20:23 +0100

    Version 1.5

--- src/main/java/dev/JustRed23/jdautils/JDAUtilities.java
+++ src/main/java/dev/JustRed23/jdautils/JDAUtilities.java
@@ -30,7 +30,7 @@ import java.util.function.Function;
  */
 public final class JDAUtilities {
 
-    private static final String version = "1.4";
+    private static final String version = "1.5";
     static Builder builder;
 
     private JDAUtilities() {}
