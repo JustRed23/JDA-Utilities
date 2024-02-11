ffefb79123aeb445251c1ea8894caadbc73085a3
JustRed23 <manuel.bormans@gmail.com>
2024-02-11 15:45:57 +0100

    Move all calls to the DataStore enum

--- src/main/java/dev/JustRed23/jdautils/data/DataStore.java
+++ src/main/java/dev/JustRed23/jdautils/data/DataStore.java
@@ -1,22 +1,85 @@
 package dev.JustRed23.jdautils.data;
 
-import dev.JustRed23.jdautils.data.impl.CustomManager;
-import dev.JustRed23.jdautils.data.impl.GuildManager;
-import dev.JustRed23.jdautils.data.impl.UserManager;
+import dev.JustRed23.jdautils.JDAUtilities;
+import org.jetbrains.annotations.CheckReturnValue;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public enum DataStore {
 
-    GUILD(GuildManager.class),
-    USER(UserManager.class),
-    CUSTOM(CustomManager.class);
+    GUILD("guild_%s_settings"),
+    USER("user_%s_settings"),
+    CUSTOM("custom_%s_settings");
 
-    private final Class<? extends Manager> clazz;
+    private final String tableName;
 
-    DataStore(Class<? extends Manager> clazz) {
-        this.clazz = clazz;
+    DataStore(String unformattedTableName) {
+        this.tableName = unformattedTableName;
     }
 
-    public Manager use() {
-        return Database.get(clazz);
+    public void createTable(long tableIdentifier) {
+        String table = tableName.formatted(tableIdentifier);
+        try (
+                var connection = JDAUtilities.getDatabaseConnection();
+                PreparedStatement stmt = connection.prepareStatement(Database.TABLE_CREATION_STRING.formatted(table))
+        ) {
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Could not create table '" + table + "'", e);
+        }
+    }
+
+    @CheckReturnValue
+    public InteractionResult get(long tableIdentifier, String setting) {
+        try (
+                var connection = JDAUtilities.getDatabaseConnection();
+                PreparedStatement stmt = connection.prepareStatement("SELECT * FROM " + tableName.formatted(tableIdentifier) + " WHERE setting = ?")
+        ) {
+            stmt.setString(1, setting);
+
+            try (ResultSet resultSet = stmt.executeQuery()) {
+                return resultSet.next() ? InteractionResult.SUCCESS.setValue(resultSet.getString("value")) : InteractionResult.NOT_FOUND;
+            }
+        } catch (SQLException e) {
+            return InteractionResult.ERROR.setError(e);
+        }
+    }
+
+    @CheckReturnValue
+    public InteractionResult insert(long tableIdentifier, String setting, String value) {
+        try (
+                var connection = JDAUtilities.getDatabaseConnection();
+                PreparedStatement stmt = connection.prepareStatement("INSERT INTO " + tableName.formatted(tableIdentifier) + " (setting, value) VALUES (?, ?)")
+        ) {
+            stmt.setString(1, setting);
+            stmt.setString(2, value);
+
+            return stmt.executeUpdate() > 0 ? InteractionResult.SUCCESS : InteractionResult.NO_CHANGE;
+        } catch (SQLException e) {
+            if (e.getErrorCode() == 19) return InteractionResult.DUPLICATE;
+            return InteractionResult.ERROR.setError(e);
+        }
+    }
+
+    @CheckReturnValue
+    public InteractionResult update(long tableIdentifier, String setting, String newValue) {
+        return null;
+    }
+
+    @CheckReturnValue
+    public InteractionResult delete(long tableIdentifier, String setting) {
+        return null;
+    }
+
+    public InteractionResult insertOrUpdate(long tableIdentifier, String setting, String value) {
+        if (has(tableIdentifier, setting))
+            return update(tableIdentifier, setting, value);
+        return insert(tableIdentifier, setting, value);
+    }
+
+    public boolean has(long tableIdentifier, String setting) {
+        return get(tableIdentifier, setting) != InteractionResult.NOT_FOUND;
     }
 }
--- src/main/java/dev/JustRed23/jdautils/data/Database.java
+++ src/main/java/dev/JustRed23/jdautils/data/Database.java
@@ -1,27 +1,8 @@
 package dev.JustRed23.jdautils.data;
 
-import org.reflections.Reflections;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
-
 class Database {
 
-    private static final List<Manager> managers = new ArrayList<>();
+    static final String TABLE_CREATION_STRING = "CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY AUTOINCREMENT, setting VARCHAR(255) UNIQUE NOT NULL, value VARCHAR(255) NOT NULL)";
 
-    static {
-        Reflections reflections = new Reflections(Database.class.getPackage().getName() + ".impl");
-        reflections.getSubTypesOf(Manager.class).forEach(manager -> {
-            try {
-                managers.add(manager.getDeclaredConstructor().newInstance());
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                throw new RuntimeException("An error occurred while trying to register default managers", e);
-            }
-        });
-    }
-
-    static Manager get(Class<? extends Manager> clazz) {
-        return managers.stream().filter(manager -> manager.getClass().equals(clazz)).findFirst().orElseThrow(() -> new IllegalArgumentException("No manager found for class " + clazz.getName()));
-    }
+    //TODO: add some caching to reduce the amount of database calls
 }
