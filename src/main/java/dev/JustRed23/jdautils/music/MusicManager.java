14cdd399d3889016709a906d45cadaad6b9a0a8e
JustRed23 <manuel.bormans@gmail.com>
2026-04-13 23:24:03 +0200

    Add voice interceptor to the music manager, this is needed for lavalink

--- src/main/java/dev/JustRed23/jdautils/music/MusicManager.java
+++ src/main/java/dev/JustRed23/jdautils/music/MusicManager.java
@@ -2,7 +2,9 @@ package dev.JustRed23.jdautils.music;
 
 import dev.JustRed23.jdautils.music.event.MusicEventListener;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.hooks.VoiceDispatchInterceptor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface MusicManager {
 
@@ -11,6 +13,14 @@ public interface MusicManager {
 
     void addEventListener(@NotNull MusicEventListener listener);
     void removeEventListener(@NotNull MusicEventListener listener);
+
+    /**
+     * Returns the voice dispatch interceptor used by this music manager, if any.
+     * <p>Implementations that do not need one can return {@code null}.</p>
+     */
+    default @Nullable VoiceDispatchInterceptor getVoiceDispatchInterceptor() {
+        return null;
+    }
 
     void destroy();
 }
