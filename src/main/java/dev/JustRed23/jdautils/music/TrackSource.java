bb94eadebd8894df569195c6ab59b42ec0781ff1
JustRed23 <manuel.bormans@gmail.com>
2026-04-16 12:19:15 +0200

    Add get method to TrackSource enum

--- src/main/java/dev/JustRed23/jdautils/music/TrackSource.java
+++ src/main/java/dev/JustRed23/jdautils/music/TrackSource.java
@@ -1,8 +1,19 @@
 package dev.JustRed23.jdautils.music;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum TrackSource {
     UNKNOWN,
     YOUTUBE,
     SPOTIFY,
-    CUSTOM
+    CUSTOM;
+
+    public static @NotNull TrackSource get(String identifier) {
+        return switch (identifier.toLowerCase()) {
+            case "youtube" -> YOUTUBE;
+            case "spotify" -> SPOTIFY;
+            case "custom" -> CUSTOM;
+            default -> UNKNOWN;
+        };
+    }
 }
